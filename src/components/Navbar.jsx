0c5bf3843dc1accfce45014e3eefbadbabef9cf7
Cristina Morales <cristimoralespadron@icloud.com>
2023-11-25 09:44:47 -0400

    FAQ in NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Flex, Box, Text, useMediaQuery} from "@chakra-ui/react"
 
 function NavBar(){
-  const [isLargerThan700] = useMediaQuery("(min-width: 700px)");
+  const [isLargerThan550] = useMediaQuery("(min-width: 550px)");
     return (
         <Flex as="nav" bg={"#007935"} color="white" align="center" justify="space-between" padding="1rem">
         <Box>
           <Text fontSize="lg" fontWeight="bold">Logo</Text>
         </Box>
-        {isLargerThan700 ? <Flex>
+        {isLargerThan550 ? <Flex>
           <Box marginLeft="2rem">
             <Text fontSize="md" as='b'>FAQ</Text>
           </Box>
@@ -19,14 +19,14 @@ function NavBar(){
             <Text fontSize="md" as='b'>RECLUTADOR</Text>
           </Box>
         </Flex> : <Flex>
-        <Box marginLeft="2rem">
-            <Text fontSize="12px" as='b'>FAQ</Text>
-          </Box>
-          <Box marginLeft="2rem">
-            <Text fontSize="12px" as='b'>EGRESADO</Text>
+        <Box marginLeft="1rem">
+            <Text fontSize="11px" as='b'>FAQ</Text>
           </Box>
           <Box marginLeft="1rem">
-            <Text fontSize="12px" as='b'>RECLUTADOR</Text>
+            <Text fontSize="11px" as='b'>EGRESADO</Text>
+          </Box>
+          <Box marginLeft="1rem">
+            <Text fontSize="11px" as='b'>RECLUTADOR</Text>
           </Box>
         </Flex> }
         
