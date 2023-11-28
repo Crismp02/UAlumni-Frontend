3e6fcd3c6ffce46a6454845e715679e331373716
Cristina Morales <cristimoralespadron@icloud.com>
2023-11-28 15:31:03 -0400

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Flex, Box, Text, useMediaQuery} from "@chakra-ui/react"
+import { Flex, Box, Text, useMediaQuery, Image} from "@chakra-ui/react"
+import Logo from '../images/logo.png'
+import Logo2 from '../images/logo2.png'
 
 function NavBar(){
   const [isLargerThan550] = useMediaQuery("(min-width: 550px)");
+  const [isLargerThan400] = useMediaQuery("(min-width: 400px)");
     return (
         <Flex as="nav" bg={"#007935"} color="white" align="center" justify="space-between" padding="1rem">
-        <Box>
-          <Text fontSize="lg" fontWeight="bold">Logo</Text>
+        <Box padding="0px">
+        {isLargerThan400 ?   
+        <Image
+            src={Logo2}
+            alt="Logo"
+            height={["25px","30px"]}
+          /> : <Image
+          src={Logo}
+          alt="Logo"
+          height={["25px","30px"]}
+        />}
         </Box>
         {isLargerThan550 ? <Flex>
           <Box marginLeft="2rem">
